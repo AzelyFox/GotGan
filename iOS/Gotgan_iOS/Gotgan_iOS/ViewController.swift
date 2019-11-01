c2249a88f9a6eab69a8c77ecd20d47101fd9dffe
7kQon <kanggakky1214@gmail.com>
2019-11-02 00:16:11 +0900

    Create iOS project

---
+++ iOS/Gotgan_iOS/Gotgan_iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Gotgan_iOS
+//
+//  Created by Gon on 02/11/2019.
+//  Copyright © 2019 reqon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
