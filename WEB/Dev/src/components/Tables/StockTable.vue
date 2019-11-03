8a5689ac862f9ece3e492a6011318cac2c6ba457
codemike@naver.com <codemike@naver.com>
2019-11-03 21:57:22 +0900

    feat - StockTable 임시 데이터 보여주기 추가

--- WEB/Dev/src/components/Tables/StockTable.vue
+++ WEB/Dev/src/components/Tables/StockTable.vue
@@ -1,39 +1,32 @@
 <template>
   <div>
     <md-table v-model="users" :table-header-color="tableHeaderColor">
-      <md-table-row slot="md-table-row" slot-scope="{ item }">
-        <md-table-cell md-label="ID">{{ item.id }}</md-table-cell>
-        <md-table-cell md-label="종류">{{ item.group }}</md-table-cell>
-        <md-table-cell md-label="보관중">{{ item.state_Store }}</md-table-cell>
-        <md-table-cell md-label="대여중">{{ item.state_Rent}}</md-table-cell>
-        <md-table-cell md-label="수리중">{{ item.state_Refair }}</md-table-cell>
-        <md-table-cell md-label="총 수량">{{ item.amount }}</md-table-cell>
+      <md-table-row>
+        <md-table-head>종류</md-table-head>
+        <md-table-head>대여 가능</md-table-head>
+        <md-table-head>사용 불가</md-table-head>
+        <md-table-head>고장</md-table-head>
+        <md-table-head>수리중</md-table-head>
+        <md-table-head>대여중</md-table-head>
+        <md-table-head>총 갯수</md-table-head>
+      </md-table-row>
+
+      <md-table-row slot="md-table-row"  v-for="item in productGroup">
+        <md-table-cell>{{ item.group_name }}</md-table-cell>
+        <md-table-cell>{{ item.group_count_available - item.group_count_rent }}</md-table-cell>
+        <md-table-cell>{{ item.group_count_unavailable}}</md-table-cell>
+        <md-table-cell>{{ item.group_count_broken}}</md-table-cell>
+        <md-table-cell>{{ item.group_count_repair}}</md-table-cell>
+        <md-table-cell>{{ item.group_count_rent}}</md-table-cell>
+        <md-table-cell>{{ item.group_count_available + item.group_count_unavailable + item.group_count_broken + item.group_count_repair}}</md-table-cell>
       </md-table-row>
     </md-table>
-<p>start</p>
-<input v-model="message" placeholder="여기를 수정해보세요">
-<p>메시지: {{ message }}</p>
-<div class="info">
-  <div class="title white-primary-hover" role="lin">{{ test }}</div>
-
-</div>
-<p>end</p>
   </div>
 </template>
 
 <script>
 import axios from 'axios';
 var params = new URLSearchParams();
-
-var exportData = function(params){
-  axios.post('https://devx.kr/Apps/GotGan/product_list.php', params)
-  .then(function(response) {
-    console.log(response);
-  })
-  .catch(function(error) {
-    console.log(error);
-  });
-};
 
 export default {
   name: "ordered-table",
@@ -46,9 +39,8 @@ export default {
   },
   data() {
     return {
-      message: [],
-      test: [],
-    errors: [],
+      productGroup: [],
+      errors: [],
       selected: [],
       users: [
         // DB에서 받아오는 대여 현황 Product_List
@@ -73,8 +65,47 @@ export default {
   },
   created(){
     params.append('session', this._props.userInfo_Table.session);
-    exportData(params);
-    console.log(this._props);
+    this.exportData(params);
+  },
+  methods:{
+    exportData: function(){
+      var vue = this;
+      axios.post('https://devx.kr/Apps/GotGan/product_overview.php', params)
+      .then(function(response) {
+        console.log(response.data);
+        for(var x = 1; x <= Object.keys(response.data.groups).length; x++){
+          vue.productGroup.push(response.data.groups[x]);
+
+          // 초기화
+          if(vue.productGroup[x-1].group_count_available == null){
+            vue.productGroup[x-1].group_count_available = 0;
+          }
+
+          if(vue.productGroup[x-1].group_count_unavailable == null){
+            vue.productGroup[x-1].group_count_unavailable = 0;
+          }
+
+          if(vue.productGroup[x-1].group_count_broken == null){
+            vue.productGroup[x-1].group_count_broken = 0;
+          }
+
+          if(vue.productGroup[x-1].group_count_repair == null){
+            vue.productGroup[x-1].group_count_repair = 0;
+          }
+
+          if(vue.productGroup[x-1].group_count_rent == null){
+            vue.productGroup[x-1].group_count_rent = 0;
+          }
+
+        }
+
+
+        console.log(vue.productGroup);
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+    }
   }
 };
 </script>
