6d06658d9caffe7a4b74a58a2e191055ef05d1f2
7kQon <kanggakky1214@gmail.com>
2019-11-04 17:26:59 +0900

    Project reset

---
+++ iOS/Gotgan_iOS/Gotgan_iOS/Network.swift
@@ -0,0 +1,9 @@
+//
+//  Network.swift
+//  Gotgan_iOS
+//
+//  Created by Gon on 02/11/2019.
+//  Copyright © 2019 reqon. All rights reserved.
+//
+
+import Foundation
---
+++ iOS/Gotgan_iOS/Gotgan_iOS/NetworkManager.swift
@@ -0,0 +1,9 @@
+//
+//  NetworkManager.swift
+//  Gotgan_iOS
+//
+//  Created by Gon on 04/11/2019.
+//  Copyright © 2019 reqon. All rights reserved.
+//
+
+import Foundation
