2ba8c3cdb70da0927d3a45d57e4f3fdcd3b519b7
codemike@naver.com <codemike@naver.com>
2019-11-07 21:23:17 +0900

    feat - 반출입 탭_대여 현황 테이블 임시 추가

--- WEB/Dev/src/components/Tables/RentStatusTable.vue
+++ WEB/Dev/src/components/Tables/RentStatusTable.vue
@@ -1,12 +1,19 @@
 <template>
   <div>
-    <md-table v-model="users" :table-header-color="tableHeaderColor">
-      <md-table-row slot="md-table-row" slot-scope="{ item }">
-        <md-table-cell md-label="이름">{{ item.name }}</md-table-cell>
-        <md-table-cell md-label="항목">{{ item.item }}</md-table-cell>
-        <md-table-cell md-label="기간">{{ item.period }}</md-table-cell>
-        <md-table-cell md-label="처리 버튼">
-          <md-button class="md-raised" data-background-color="red">반납 확인</md-button>
+    <md-table :table-header-color="tableHeaderColor">
+      <md-table-row>
+        <md-table-head>이름</md-table-head>
+        <md-table-head>항목</md-table-head>
+        <md-table-head>시작일</md-table-head>
+        <md-table-head>처리 버튼</md-table-head>
+      </md-table-row>
+      <md-table-row slot="md-table-row" v-for="item in rentList" v-if="item.rent_status == 2">
+        <md-table-cell>{{ item.rent_user_name }}</md-table-cell>
+        <md-table-cell>{{ item.rent_product_name }}</md-table-cell>
+        <md-table-cell>{{ item.rent_time_start }}</md-table-cell>
+        <md-table-cell>
+          <md-button class="md-raised" data-background-color="blue">허가</md-button>
+          <md-button class="md-raised" data-background-color="red">거부</md-button>
         </md-table-cell>
       </md-table-row>
     </md-table>
@@ -14,25 +21,48 @@
 </template>
 
 <script>
+import axios from 'axios';
+
+var params = new URLSearchParams();
+
 export default {
   name: "rent-status-table",
   props: {
     tableHeaderColor: {
       type: String,
       default: ""
-    }
+    },
+    userInfo_Table: Object
   },
   data() {
     return {
       selected: [],
-      users: [
-        {
-          name: "김아무개",
-          item: "3D 프린터",
-          period: "19.10.16 ~ 19.10.23"
-        }
-      ]
+      rentList: []
     };
+  },
+  created(){
+    console.log("RentRequestTable");
+    console.log(this._props);
+    params.append('session', this._props.userInfo_Table.session);
+    this.exportData(params);
+  },
+  methods:{
+    exportData: function(){
+      var vue = this;
+      axios.post('https://api.devx.kr/GotGan/v1/rent_list.php', params)
+      .then(function(response) {
+        console.log(response.data);
+        for(var x = 0; x < Object.keys(response.data.rents).length; x++){
+          vue.rentList.push(response.data.rents[x]);
+        }
+
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+    }
   }
 };
+
+
 </script>
