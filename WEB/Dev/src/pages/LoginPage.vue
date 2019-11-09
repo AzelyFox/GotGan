e099794efa762e61978391ee6a206dadce42aa8c
codemike@naver.com <codemike@naver.com>
2019-11-10 00:35:45 +0900

    feat - 로그인 페이지_회원가입 폼 추가

--- WEB/Dev/src/pages/LoginPage.vue
+++ WEB/Dev/src/pages/LoginPage.vue
@@ -1,27 +1,67 @@
 <template>
-  <form novalidate class="md-layout" @submit.prevent="validateUser">
-    <md-card class="md-layout-item md-size-50 md-small-size-100">
-      <md-card-content>
-        <div class="md-layout md-gutter">
-          <div class="md-layout-item md-small-size-100">
-            <md-field :class="messageClass" :md-counter="false">
-              <label for="id">ID</label>
-              <md-input name="id" v-model="userID" maxlength="20" required/>
-              <span class="md-error">There is an error</span>
-            </md-field>
-            <md-field :class="messageClass" :md-counter="false">
-              <label for="pw">PW</label>
-              <md-input name="pw" v-model="userPW" type="password"  maxlength="20" required/>
-              <span class="md-error">There is an error</span>
-            </md-field>
+      <form novalidate class="md-layout" @submit.prevent="validateUser">
+        <div class="md-layout-item md-size-35"></div>
+        <md-card class="md-layout-item md-size-30 md-small-size-100 md-gutter card">
+          <md-card-header>
+            <span class="md-title">GotGan</span>
+          </md-card-header>
+          <md-card-content>
+            <div class="md-layout md-gutter">
+              <div class="md-layout-item md-small-size-100 cardDiv">
+                <md-field :class="messageClass" :md-counter="false">
+                  <label for="userID">ID</label>
+                  <md-input name="userID" v-model="user_ID" maxlength="20" required/>
+                  <span class="md-error">There is an error</span>
+                </md-field>
 
-            <md-button class="md-raised" v-on:click="signIn" :to="link">Sign in</md-button>
+                <md-field :class="messageClass" :md-counter="false">
 
-          </div>
-        </div>
-      </md-card-content>
-    </md-card>
-  </form>
+                  <label for="userPW">Password</label>
+                  <md-input name="userPW" v-model="user_Password" type="password"  maxlength="20" required/>
+                  <span class="md-error">There is an error</span>
+                </md-field>
+
+                <transition-group name="slide-fade">
+                  <div v-bind:key="showCard">
+                    <md-field :class="messageClass" :md-counter="false" v-if="showCard">
+                      <label for="userLevel">권한</label>
+                      <md-input name="userLevel" v-model="user_Level" maxlength="20" required/>
+                      <span class="md-error">There is an error</span>
+                    </md-field>
+                    <md-field :class="messageClass" :md-counter="false" v-if="showCard">
+                      <label for="userName">이름</label>
+                      <md-input name="userName" v-model="user_Name" maxlength="20" required/>
+                      <span class="md-error">There is an error</span>
+                    </md-field>
+                    <md-field :class="messageClass" :md-counter="false" v-if="showCard">
+                      <label for="userGroup">유저그룹</label>
+                      <md-input name="userGroup" v-model="user_Group" maxlength="20"/>
+                    </md-field>
+                    <md-field :class="messageClass" :md-counter="false" v-if="showCard">
+                      <label for="userSID">학번</label>
+                      <md-input name="userSID" v-model="user_SID" maxlength="20"/>
+                    </md-field>
+                    <md-field :class="messageClass" :md-counter="false" v-if="showCard">
+                      <label for="userEmail">이메일</label>
+                      <md-input name="userEmail" v-model="user_Email" maxlength="20"/>
+                    </md-field>
+                    <md-field :class="messageClass" :md-counter="false" v-if="showCard">
+                      <label for="user_Phone">전화번호</label>
+                      <md-input name="user_Phone" v-model="user_Phone" maxlength="20"/>
+                    </md-field>
+                  </div>
+                </transition-group>
+              </div>
+            </div>
+          </md-card-content>
+          <md-card-actions>
+            <md-button class="md-raised" v-on:click="signIn" :to="link" v-if="showCard == 0">Sign in</md-button>
+            <md-button v-on:click="showCard = 1" v-if="showCard == 0">Sign up</md-button>
+            <md-button v-on:click="showCard = 0" v-if="showCard == 1">Register</md-button>
+          </md-card-actions>
+        </md-card>
+        <div class="md-layout-item md-size-35"></div>
+      </form>
 </template>
 
 <script>
@@ -47,10 +87,17 @@ export default {
   },
   data(){
     return{
-      userID: "",
-      userPW: "",
+      user_ID: "",
+      user_Password: "",
       link: "",
-      hasMessages: false
+      hasMessages: false,
+      showCard: 0,
+      user_Level: "",
+      user_Name: "",
+      user_Group: "",
+      user_SID: "",
+      user_Email: "",
+      user_Phone: ""
     }
   },
   methods: {
@@ -58,8 +105,8 @@ export default {
       if(this.userID == "" || this.userPW == ""){
         this.hasMessages = true;
       }else{
-        params.append('user_id', this.userID);
-        params.append('user_pw', this.userPW);
+        params.append('user_id', this.user_ID);
+        params.append('user_pw', this.user_Password);
         exportData(params, this);
       }
     }
@@ -79,3 +126,26 @@ export default {
 };
 
 </script>
+
+<style>
+  .card {
+    margin-top: 12rem!important;
+  }
+
+  .cardDiv {
+    transition-property: all;
+    transition-duration: 1s;
+  }
+
+  .slide-fade-enter-active {
+    transition: all .3s ease;
+  }
+  .slide-fade-leave-active {
+    transition: all .3s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+  }
+  .slide-fade-enter, .slide-fade-leave-to
+  /* .slide-fade-leave-active below version 2.1.8 */ {
+  transform: translateX(10px);
+  opacity: 0;
+  }
+</style>
