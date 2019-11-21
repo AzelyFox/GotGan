714693032ffc0810060cb229dc9015534c6a6444
codemike@naver.com <codemike@naver.com>
2019-11-21 21:29:23 +0900

    feat - 전체 새로 고침시 쿠키사용으로 재로그인 기능 추가

--- WEB/Dev/src/pages/Layout/DashboardLayout.vue
+++ WEB/Dev/src/pages/Layout/DashboardLayout.vue
@@ -48,6 +48,9 @@
 </template>
 <style lang="scss"></style>
 <script>
+import router from '../../main.js'
+import axios from 'axios';
+
 import TopNavbar from "./TopNavbar.vue";
 import ContentFooter from "./ContentFooter.vue";
 import DashboardContent from "./Content.vue";
@@ -66,15 +69,67 @@ export default {
   data(){
     return{
       userInfo: {},
-      user_Level: 0
+      user_Level: 0,
+      session: ""
     }
   },
   created(){
     console.log("DashboardLayout");
-    console.log(this._props);
-    this.user_Level = this._props._userInfo.user_level;
-    this.userInfo = this._props._userInfo;
-    //console.log(this.userInfo);
+    console.log(this._props._userInfo);
+    this.session = this.getCookie("session");
+
+    if(Object.keys(this._props._userInfo).length == 0){
+      this.login();
+    }else{
+      this.user_Level = this._props._userInfo.user_level;
+      this.userInfo = this._props._userInfo;
+    }
+
+
+  },
+  methods: {
+    getCookie: function(_name) {
+      var value = document.cookie.match('(^|;) ?' + _name + '=([^;]*)(;|$)');
+      return value? value[2] : null;
+    },
+    login : function(){
+      var signInParams = new URLSearchParams();
+      var vue = this;
+      signInParams.append('session', this.session);
+      console.log(this.session);
+
+      axios.post('https://api.devx.kr/GotGan/v1/login.php', signInParams)
+      .then(function(response) {
+        if(response.data.result == 0){
+          // 로그인 성공
+          vue.user_Level = response.data.user_level;
+          vue.userInfo = {
+            error: response.data.error,
+            result: response.data.result,
+            session: response.data.session,
+            user_block: response.data.user_block,
+            user_created: response.data.user_created,
+            user_email: response.data.user_email,
+            user_group_index: response.data.user_group_index,
+            user_group_name: response.data.user_group_name,
+            user_id: response.data.user_id,
+            user_index: response.data.user_index,
+            user_level: response.data.user_level,
+            user_name: response.data.user_name,
+            user_phone: response.data.user_phone,
+            user_sid: response.data.user_sid
+          };
+        }else{
+          // 로그인 실패
+          alert("다시 로그인 하시오.");
+          router.push("/login");
+        }
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+
+    },
   }
 };
 </script>
