9870ff71715676d64ec4b97f091f7d4ddebda905
codemike@naver.com <codemike@naver.com>
2019-11-23 12:30:49 +0900

    feat - 재고 상세_각 재고별 로그 띄우기, 테이블 스크롤바 추가

--- WEB/Dev/src/components/Tables/StockDetailTable.vue
+++ WEB/Dev/src/components/Tables/StockDetailTable.vue
@@ -13,33 +13,35 @@
           <span class="list-header">총 갯수</span>
         </div>
       </md-list-item>
+      <md-content class="md-scrollbar tableScrollDiv">
+        <md-list-item v-for="item in productGroup">
+          <div class="list-line" @click="toggle(item)">
+            <span class="list-item">{{ item.group_name }}</span>
+            <span class="list-item">{{ item.group_count_available - item.group_count_rent }}</span>
+            <span class="list-item">{{ item.group_count_unavailable}}</span>
+            <span class="list-item">{{ item.group_count_broken}}</span>
+            <span class="list-item">{{ item.group_count_repair}}</span>
+            <span class="list-item">{{ item.group_count_rent}}</span>
+            <span class="list-item">{{ item.group_count_available + item.group_count_unavailable + item.group_count_broken + item.group_count_repair}}</span>
+          </div>
+          <md-table v-show="item.showTable">
+            <md-table-row>
+              <md-table-head md-numeric>바코드ID</md-table-head>
+              <md-table-head>이름</md-table-head>
+              <md-table-head>소속</md-table-head>
+              <md-table-head>구매 일자</md-table-head>
+            </md-table-row>
 
-      <md-list-item v-for="item in productGroup">
-        <div class="list-line" @click="toggle(item)">
-          <span class="list-item">{{ item.group_name }}</span>
-          <span class="list-item">{{ item.group_count_available - item.group_count_rent }}</span>
-          <span class="list-item">{{ item.group_count_unavailable}}</span>
-          <span class="list-item">{{ item.group_count_broken}}</span>
-          <span class="list-item">{{ item.group_count_repair}}</span>
-          <span class="list-item">{{ item.group_count_rent}}</span>
-          <span class="list-item">{{ item.group_count_available + item.group_count_unavailable + item.group_count_broken + item.group_count_repair}}</span>
-        </div>
-        <md-table v-show="item.showTable">
-          <md-table-row>
-            <md-table-head md-numeric>바코드ID</md-table-head>
-            <md-table-head>이름</md-table-head>
-            <md-table-head>소속</md-table-head>
-            <md-table-head>구매 일자</md-table-head>
-          </md-table-row>
+            <md-table-row v-for="product in item.products" @click="toggleDialog(product)">
+              <md-table-cell md-numeric>{{ product.product_barcode }}</md-table-cell>
+              <md-table-cell>{{ product.product_name }}</md-table-cell>
+              <md-table-cell>{{ product.product_owner_name }}</md-table-cell>
+              <md-table-cell>{{ product.product_created }}</md-table-cell>
+            </md-table-row>
 
-          <md-table-row v-for="product in item.products" @click="toggleDialog(product)">
-            <md-table-cell md-numeric>{{ product.product_barcode }}</md-table-cell>
-            <md-table-cell>{{ product.product_name }}</md-table-cell>
-            <md-table-cell>{{ product.product_owner_name }}</md-table-cell>
-            <md-table-cell>{{ product.product_created }}</md-table-cell>
-          </md-table-row>
-        </md-table>
-      </md-list-item>
+          </md-table>
+        </md-list-item>
+      </md-content>
     </md-list>
 
 
@@ -47,40 +49,40 @@
     <md-dialog :md-active.sync="showDialog">
       <md-dialog-title>제품 상세</md-dialog-title>
       <md-dialog-content>
-        <md-table :table-header-color="tableHeaderColor">
-          <md-table-row>
-            <md-table-head md-numeric>바코드ID</md-table-head>
-            <md-table-head>이름</md-table-head>
-            <md-table-head>소속</md-table-head>
-            <md-table-head>구매 일자</md-table-head>
-          </md-table-row>
+        <div>바코드 ID : {{ dialogInfo.product_barcode }}</div>
+        <div>이름 : {{ dialogInfo.product_name }}</div>
+        <div>소속 : {{ dialogInfo.product_owner_name }}</div>
+        <div>구매 일자 : {{ dialogInfo.product_created }}</div>
+        <div>로그</div>
+        <md-content class="md-scrollbar logScrollDiv">
+          <md-table :table-header-color="tableHeaderColor">
+            <md-table-row>
+              <md-table-head>생성 시간</md-table-head>
+              <md-table-head>로그 타입</md-table-head>
+              <md-table-head>생성 유저 ID</md-table-head>
+              <md-table-head>생성 유저 이름</md-table-head>
+            </md-table-row>
 
-          <md-table-row>
-            <md-table-cell md-numeric>{{ dialogInfo.product_barcode }}</md-table-cell>
-            <md-table-cell>{{ dialogInfo.product_name }}</md-table-cell>
-            <md-table-cell>{{ dialogInfo.product_owner_name }}</md-table-cell>
-            <md-table-cell>{{ dialogInfo.product_created }}</md-table-cell>
-          </md-table-row>
-          <!--
-          product_barcode: 0
-          product_created: "2019-11-05 17:59:05"
-          product_group_index: 1
-          product_group_name: "3D 프린터"
-          product_group_priority: 0
-          product_index: 32
-          product_name: "3D 프린터 (대형)"
-          product_owner_index: 1
-          product_owner_name: "총무부"
-          product_rent_index: 0
-          product_status: 0
-        -->
-      </md-table>
-    </md-dialog-content>
-    <md-dialog-actions>
-      <md-button class="md-primary" @click="showDialog = false">Close</md-button>
-    </md-dialog-actions>
-  </md-dialog>
-</div>
+            <md-table-row v-for="log in logList">
+              <md-table-cell>{{ log.time }}</md-table-cell>
+              <md-table-cell>{{ log.type }}</md-table-cell>
+              <md-table-cell>{{ log.userID }}</md-table-cell>
+              <md-table-cell>{{ log.userName }}</md-table-cell>
+            </md-table-row>
+
+            <md-table-row v-if="logList.length == 0">
+              <md-table-cell>로그가 없습니다.</md-table-cell>
+            </md-table-row>
+
+          </md-table>
+        </md-content>
+
+      </md-dialog-content>
+      <md-dialog-actions>
+        <md-button class="md-primary" @click="showDialog = false">Close</md-button>
+      </md-dialog-actions>
+    </md-dialog>
+  </div>
 </template>
 
 <script>
@@ -236,5 +238,15 @@ export default {
 .list-item {
   height: fit-content;
   padding: 1rem 0;
+}
+
+.tableScrollDiv{
+  max-height: 30rem;
+  overflow: auto;
+}
+
+.logScrollDiv {
+  max-height: 20rem;
+  overflow: auto;
 }
 </style>
