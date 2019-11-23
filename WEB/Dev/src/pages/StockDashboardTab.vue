c81766df7a1cf411a627e01c1ab805b8d540874f
codemike@naver.com <codemike@naver.com>
2019-11-23 09:49:44 +0900

    feat - 재고 대시보드_차트 카드 추가

--- WEB/Dev/src/pages/StockDashboardTab.vue
+++ WEB/Dev/src/pages/StockDashboardTab.vue
@@ -20,13 +20,57 @@
         </md-card>
       </div>
 
+      <div class="md-layout-item md-size-50">
+        <chart-card
+        :chart-data="dailySalesChart.data"
+        :chart-options="dailySalesChart.options"
+        chart-type="Line"
+        data-background-color="green">
+        <template slot="content">
+          <h4 class="title">대여 현황</h4>
+          <p class="category">
+            <span class="text-success"><i class="fa fa-long-arrow-up"></i> 55% </span> 전월 대비 증가
+          </p>
+        </template>
+
+        <template slot="footer">
+          <div class="stats">
+            <md-icon>access_time</md-icon>
+            4분 전 업데이트
+          </div>
+        </template>
+      </chart-card>
+    </div>
+
+    <div class="md-layout-item md-size-50">
+      <chart-card
+      :chart-data="dailySalesChart2.data"
+      :chart-options="dailySalesChart2.options"
+      chart-type="Line"
+      data-background-color="red">
+      <template slot="content">
+        <h4 class="title">반납 연채 현황</h4>
+        <p class="category">
+          <span class="text-success"><i class="fa fa-long-arrow-up"></i> 10% </span> 전월 대비 감소
+        </p>
+      </template>
+
+      <template slot="footer">
+        <div class="stats">
+          <md-icon>access_time</md-icon>
+          4분 전 업데이트
+        </div>
+      </template>
+    </chart-card>
     </div>
   </div>
+</div>
 </template>
 
 <script>
 import {
-  StockTable
+  StockTable,
+  ChartCard
 } from "@/components";
 
 export default {
@@ -34,10 +78,53 @@ export default {
     userInfo_Tab: Object
   },
   components: {
-    StockTable
+    StockTable,
+    ChartCard
   },
   data() {
     return {
+      dailySalesChart: {
+        data: {
+          labels: ['May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov'],
+          series: [
+            [12, 17, 7, 17, 23, 18, 38]
+          ]
+        },
+        options: {
+          lineSmooth: this.$Chartist.Interpolation.cardinal({
+            tension: 0
+          }),
+          low: 0,
+          high: 50, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
+          chartPadding: {
+            top: 0,
+            right: 0,
+            bottom: 0,
+            left: 0
+          }
+        }
+      },
+      dailySalesChart2: {
+        data: {
+          labels: ['May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov'],
+          series: [
+            [15, 12, 7, 5, 8, 10, 9]
+          ]
+        },
+        options: {
+          lineSmooth: this.$Chartist.Interpolation.cardinal({
+            tension: 0
+          }),
+          low: 0,
+          high: 20, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
+          chartPadding: {
+            top: 0,
+            right: 0,
+            bottom: 0,
+            left: 0
+          }
+        }
+      }
     };
   },
   created(){
