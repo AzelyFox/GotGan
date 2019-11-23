db091dcbecd0899ca0ed5f93abf92cbccd3caa71
codemike@naver.com <codemike@naver.com>
2019-11-23 09:47:29 +0900

    feat - 대여 대시보드_테이블 쿠키 기능 적용

--- WEB/Dev/src/components/Tables/RentRequestTable.vue
+++ WEB/Dev/src/components/Tables/RentRequestTable.vue
@@ -5,15 +5,17 @@
         <md-table-head>이름</md-table-head>
         <md-table-head>항목</md-table-head>
         <md-table-head>시작일</md-table-head>
+        <md-table-head>반납 예정일</md-table-head>
         <md-table-head>처리 버튼</md-table-head>
       </md-table-row>
       <md-table-row slot="md-table-row" v-for="item in rentList" v-if="item.rent_status == 1">
         <md-table-cell>{{ item.rent_user_name }}</md-table-cell>
         <md-table-cell>{{ item.rent_product_name }}</md-table-cell>
         <md-table-cell>{{ item.rent_time_start }}</md-table-cell>
+        <md-table-cell>{{ item.rent_time_end }}</md-table-cell>
         <md-table-cell>
-          <md-button class="md-raised" data-background-color="blue">허가</md-button>
-          <md-button class="md-raised" data-background-color="red">거부</md-button>
+          <md-button class="md-raised" data-background-color="blue" @click="allowButton(item.rent_index)">허가</md-button>
+          <md-button class="md-raised" data-background-color="red" @click="">거부</md-button>
         </md-table-cell>
       </md-table-row>
     </md-table>
@@ -43,7 +45,7 @@ export default {
   created(){
     console.log("RentRequestTable");
     console.log(this._props);
-    params.append('session', this._props.userInfo_Table.session);
+    params.append('session', this.getCookie("session"));
     this.exportData(params);
   },
   methods:{
@@ -60,7 +62,29 @@ export default {
       .catch(function(error) {
         console.log(error);
       });
-    }
+    },
+    getCookie: function(_name) {
+      var value = document.cookie.match('(^|;) ?' + _name + '=([^;]*)(;|$)');
+      return value? value[2] : null;
+    },
+    allowButton: function(_index) {
+      console.log(_index);
+      var vue = this;
+      var allowParams = new URLSearchParams();
+      allowParams.append('session', this.getCookie("session"));
+      allowParams.append('rent_index', _index);
+
+
+      axios.post('https://api.devx.kr/GotGan/v1/rent_allow.php', allowParams)
+      .then(function(response) {
+        console.log(response.data);
+        vue.$forceUpdate();
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+    },
+
   }
 };
 
--- WEB/Dev/src/components/Tables/RentStatusTable.vue
+++ WEB/Dev/src/components/Tables/RentStatusTable.vue
@@ -5,15 +5,16 @@
         <md-table-head>이름</md-table-head>
         <md-table-head>항목</md-table-head>
         <md-table-head>시작일</md-table-head>
+        <md-table-head>반납예정일</md-table-head>
         <md-table-head>처리 버튼</md-table-head>
       </md-table-row>
       <md-table-row slot="md-table-row" v-for="item in rentList" v-if="item.rent_status == 2">
         <md-table-cell>{{ item.rent_user_name }}</md-table-cell>
         <md-table-cell>{{ item.rent_product_name }}</md-table-cell>
         <md-table-cell>{{ item.rent_time_start }}</md-table-cell>
+        <md-table-cell>{{ item.rent_time_end }}</md-table-cell>
         <md-table-cell>
-          <md-button class="md-raised" data-background-color="blue">허가</md-button>
-          <md-button class="md-raised" data-background-color="red">거부</md-button>
+          <md-button class="md-raised" data-background-color="red" @click="confirmButton(item.rent_index)">반납 확인</md-button>
         </md-table-cell>
       </md-table-row>
     </md-table>
@@ -43,7 +44,7 @@ export default {
   created(){
     console.log("RentRequestTable");
     console.log(this._props);
-    params.append('session', this._props.userInfo_Table.session);
+    params.append('session', this.getCookie("session"));
     this.exportData(params);
   },
   methods:{
@@ -60,6 +61,24 @@ export default {
       .catch(function(error) {
         console.log(error);
       });
+    },
+    confirmButton: function(index){
+      var vue = this;
+      var returnParams = new URLSearchParams();
+      returnParams.append("session", this.getCookie("session"));
+      returnParams.append("rent_index", index);
+
+      axios.post('https://api.devx.kr/GotGan/v1/rent_return.php', returnParams)
+      .then(function(response) {
+        console.log(response.data);
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+    },
+    getCookie: function(_name) {
+      var value = document.cookie.match('(^|;) ?' + _name + '=([^;]*)(;|$)');
+      return value? value[2] : null;
     }
   }
 };
